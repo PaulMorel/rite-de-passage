cc5381f04c73dda9e450846f5c84c319bceb2ff8
Paul Morel <paul.morel@gmail.com>
2014-03-04 15:59:58 -0500

    Updated general structure
    Also added basic JSON file for data storing

---
+++ assets/js/test.js
@@ -0,0 +1,2 @@
+document.write('<script src="http://code.jquery.com/jquery-1.11.0.min.js"><\/script>');
+document.write('<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.min.js"><\/script>');
