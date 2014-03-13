8653e954863a9eab1c12dff692cc3eb818e5a277
Paul Morel <paul.morel@gmail.com>
2014-03-13 17:40:53 -0400

    Added JSON GET request for graduates section

---
+++ assets/js/main.js
@@ -0,0 +1,83 @@
+/*
+ // modified Isotope methods for gutters in masonry
+	$.Isotope.prototype._getMasonryGutterColumns = function() {
+		var gutter = this.options.masonry && this.options.masonry.gutterWidth || 0;
+				containerWidth = this.element.width();
+	
+		this.masonry.columnWidth = this.options.masonry && this.options.masonry.columnWidth ||
+									// or use the size of the first item
+									this.$filteredAtoms.outerWidth(true) ||
+									// if there's no items, use size of container
+									containerWidth;
+
+		this.masonry.columnWidth += gutter;
+
+		this.masonry.cols = Math.floor( ( containerWidth + gutter ) / this.masonry.columnWidth );
+		this.masonry.cols = Math.max( this.masonry.cols, 1 );
+	};
+
+	$.Isotope.prototype._masonryReset = function() {
+		// layout-specific props
+		this.masonry = {};
+		// FIXME shouldn't have to call this again
+		this._getMasonryGutterColumns();
+		var i = this.masonry.cols;
+		this.masonry.colYs = [];
+		while (i--) {
+			this.masonry.colYs.push( 0 );
+		}
+	};
+
+	$.Isotope.prototype._masonryResizeChanged = function() {
+		var prevSegments = this.masonry.cols;
+		// update cols/rows
+		this._getMasonryGutterColumns();
+		// return if updated cols/rows is not equal to previous
+		return ( this.masonry.cols !== prevSegments );
+	};
+*/
+function getMajor(data) {
+	switch (data){
+		case 'design':
+			return 'Design graphique';
+			break;
+		case 'bd':
+			return 'Bande desinée';
+			break;
+		case 'museo':
+			return 'Muséologie et patrimoines';
+			break;
+		case 'art':
+			return 'Arts visuels';
+			break;
+	}
+
+	console.log(data);
+}
+
+$(document).ready(function() {
+
+
+
+	// Templates
+	var graduateTemplateMarkup = '<li class="major-${major} grid-${grid}">\
+					<a href="${niceName}" title="${firstName} ${lastName}">\
+						<h3>${firstName} ${lastName}</h3>\
+						<p>${getMajor(major)}</p>\
+						<!--<img src="" alt="">-->\
+					</a>\
+				</li>';
+
+	// Compile the markup as a named template
+	$.template('graduateTemplate', graduateTemplateMarkup);
+
+	// AJAX Requests
+	$.ajax({
+		dataType: 'json',
+		url: 'data.json',
+		success: function(data) {
+			// Render the template with the movies data and insert
+			$.tmpl( 'graduateTemplate', data ).appendTo( "section.graduates ul" );
+		}
+	});
+});
