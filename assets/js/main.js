23657cfdad2bb8573d858540353a6399e7a1865c
Paul Morel <paul.morel@gmail.com>
2014-03-14 00:04:09 -0400

    Converting CSS to mobile first
    Student CSS adjustments

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,3 @@
-/*
- // modified Isotope methods for gutters in masonry
-	$.Isotope.prototype._getMasonryGutterColumns = function() {
-		var gutter = this.options.masonry && this.options.masonry.gutterWidth || 0;
-				containerWidth = this.element.width();
-	
-		this.masonry.columnWidth = this.options.masonry && this.options.masonry.columnWidth ||
-									// or use the size of the first item
-									this.$filteredAtoms.outerWidth(true) ||
-									// if there's no items, use size of container
-									containerWidth;
-
-		this.masonry.columnWidth += gutter;
-
-		this.masonry.cols = Math.floor( ( containerWidth + gutter ) / this.masonry.columnWidth );
-		this.masonry.cols = Math.max( this.masonry.cols, 1 );
-	};
-
-	$.Isotope.prototype._masonryReset = function() {
-		// layout-specific props
-		this.masonry = {};
-		// FIXME shouldn't have to call this again
-		this._getMasonryGutterColumns();
-		var i = this.masonry.cols;
-		this.masonry.colYs = [];
-		while (i--) {
-			this.masonry.colYs.push( 0 );
-		}
-	};
-
-	$.Isotope.prototype._masonryResizeChanged = function() {
-		var prevSegments = this.masonry.cols;
-		// update cols/rows
-		this._getMasonryGutterColumns();
-		// return if updated cols/rows is not equal to previous
-		return ( this.masonry.cols !== prevSegments );
-	};
-*/
 function getMajor(data) {
 	switch (data){
 		case 'design':
@@ -57,7 +19,8 @@ function getMajor(data) {
 
 $(document).ready(function() {
 
-
+	// Templating System
+	// ==================
 
 	// Templates
 	var graduateTemplateMarkup = '<li class="major-${major} grid-${grid}">\
@@ -71,13 +34,24 @@ $(document).ready(function() {
 	// Compile the markup as a named template
 	$.template('graduateTemplate', graduateTemplateMarkup);
 
-	// AJAX Requests
+	// Get JSON Data
 	$.ajax({
 		dataType: 'json',
 		url: 'data.json',
 		success: function(data) {
-			// Render the template with the movies data and insert
+			// Render the template with the data and append to element
 			$.tmpl( 'graduateTemplate', data ).appendTo( "section.graduates ul" );
 		}
+	}).done(function(){
+		// Adaptive Grid Layout
+		// ==================
+		var isotopeContainer = $('.layout-grid ul');
+
+		// Isotope Initialisation
+		isotopeContainer.isotope({
+			// Options
+			itemSelector: 'li',
+			layoutMode: 'masonry'
+		});
 	});
 });
