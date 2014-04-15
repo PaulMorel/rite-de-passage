074a71d1b76e1d5422d4a520213859832f6dd5bc
Paul Morel <paul.morel@gmail.com>
2014-04-15 16:31:24 -0400

    Updated typographical errors

--- build/index.php
+++ build/index.php
@@ -11,23 +11,23 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
 	
 	<!-- Meta -->
-	<title>Catalogue des Finissants 2013–2014 | École Multidisciplinaire de l&#8217;Image | UQO</title>
-	<meta name="description" content="Le catalogue des finissants 2013–2014 des finisants de l'ÉMI. L&#8217;expo aura lieu le 24 avril à l&#8217;Université du Québec en Outaouais - Pavillon Lucien-Brault." />
+	<title>Catalogue des Finissants 2013–2014 | École multidisciplinaire de l&#8217;image | UQO</title>
+	<meta name="description" content="Le catalogue des finissants 2013–2014 des finisants de l&#8217;ÉMI. L&#8217;expo aura lieu le 24 avril à l&#8217;Université du Québec en Outaouais - Pavillon Lucien-Brault." />
 	<meta name="keywords" content="arts, visuels, design, graphique, muséologie, patrimoines, bande dessinée, bd, catalogue, expo, exposition, finissants, emi, émi, université, quebec, outaouais, uqo, école, multidisciplinaire, image, 2013, 2014" />
-	<meta name="author" content="École Multidisciplinaire de l&#8217;Image" />
+	<meta name="author" content="École multidisciplinaire de l&#8217;image" />
 	
 	<!-- Twitter Card data -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:site" content="@EMI_uqo" />
 	<meta name="twitter:site:id" content="2163445381" />
-	<meta name="twitter:title" content="Catalogue des Finissants 2013–2014 | École Multidisciplinaire de l&#8217;Image | UQO" />
+	<meta name="twitter:title" content="Catalogue des Finissants 2013–2014 | École multidisciplinaire de l&#8217;image | UQO" />
 	<meta name="twitter:description" content="Le catalogue des finissants 2013–2014 des finisants de l&#8217;ÉMI. L&#8217;expo aura lieu le 24 avril à l&#8217;Université du Québec en Outaouais - Pavillon Lucien-Brault." />
 	<meta name="twitter:creator" content="@EMI_uqo" />
 	<meta name="twitter:creator:id" content="2163445381" />
 	<meta name="twitter:image:src" content="assets/img/glyph.png" />
 		
 	<!-- Open Graph -->
-	<meta property="og:title" content="Catalogue des Finissants 2013–2014 | École Multidisciplinaire de l&#8217;Image | UQO" />
+	<meta property="og:title" content="Catalogue des Finissants 2013–2014 | École multidisciplinaire de l&#8217;image | UQO" />
 	<meta property="og:description" content="Le catalogue des finissants 2013–2014 des finisants de l&#8217;ÉMI. L&#8217;expo aura lieu le 24 avril à l&#8217;Université du Québec en Outaouais - Pavillon Lucien-Brault." />
 	<meta property="og:image" content="assets/img/glyph.png" />
 	<meta property="og:site_name" content="UQO | Université du Québec en Outaouais" />
@@ -67,19 +67,19 @@
 		<div class="inner">
 			<article id="art">
 				<h3>Arts Visuels</h3>
-				<p>La majeure en arts visuels vise à former des artistes professionnels et des acteurs dans ce domaine, maîtrisant les multiples aspects de la recherche-création, de la production et de la diffusion. Elle permet donc aux étudiants d'approfondir, selon le niveau de formation, tous les aspects de la création artistique, de la première idée à l'exposition d'un corpus d'oeuvres. </p>
+				<p>La majeure en arts visuels vise à former des artistes professionnels et des acteurs dans ce domaine, maîtrisant les multiples aspects de la recherche-création, de la production et de la diffusion. Elle permet donc aux étudiants d&#8217;approfondir, selon le niveau de formation, tous les aspects de la création artistique, de la première idée à l&#8217;exposition d&#8217;un corpus d&#8217;oeuvres. </p>
 			</article>
 			<article id="bd">
 				<h3>Bande Dessinée</h3>
-				<p>La majeure en bande dessinée a pour but de former des auteurs complets, maîtrisant les multiples aspects de la narration graphique. Elle permet donc aux étudiants d'approfondir tous les aspects de la création d'une bande dessinée, de la première idée à la réalisation d'une publication, de développer une réflexion sur le médium ainsi qu'un esprit critique sur la production contemporaine tout en explorant ses différents axes de développement.</p>
+				<p>La majeure en bande dessinée a pour but de former des auteurs complets, maîtrisant les multiples aspects de la narration graphique. Elle permet donc aux étudiants d&#8217;approfondir tous les aspects de la création d&#8217;une bande dessinée, de la première idée à la réalisation d&#8217;une publication, de développer une réflexion sur le médium ainsi qu&#8217;un esprit critique sur la production contemporaine tout en explorant ses différents axes de développement.</p>
 			</article>
 			<article id="design">
 				<h3>Design Graphique</h3>
-				<p>La majeure en design graphique vise à former des concepteurs graphiques imaginatifs et compétents, possédant des aptitudes pour l'analyse, la conceptualisation et la réalisation de projets. Cette formation favorise l'acquisition d'une solide culture du domaine par l'approfondissement des fondements pratiques, du processus conceptuel et de la méthodologie de production. </p>
+				<p>La majeure en design graphique vise à former des concepteurs graphiques imaginatifs et compétents, possédant des aptitudes pour l&#8217;analyse, la conceptualisation et la réalisation de projets. Cette formation favorise l&#8217;acquisition d&#8217;une solide culture du domaine par l&#8217;approfondissement des fondements pratiques, du processus conceptuel et de la méthodologie de production. </p>
 			</article>
 			<article id="museo">
 				<h3>Muséologie et Patrimoines</h3>
-				<p>La majeure en muséologie et patrimoines permet à l'étudiant d'acquérir une connaissance générale de la muséologie et des méthodes de travail dans les musées. Elle prépare les étudiants à assumer divers rôles dans le vaste champ des musées et des patrimoines. La nature de ces postes varie, elle est liée à la recherche, à la gestion des collectictions, à la communication, à l'administration ou à l'éducation.</p>
+				<p>La majeure en muséologie et patrimoines permet à l&#8217;étudiant d&#8217;acquérir une connaissance générale de la muséologie et des méthodes de travail dans les musées. Elle prépare les étudiants à assumer divers rôles dans le vaste champ des musées et des patrimoines. La nature de ces postes varie, elle est liée à la recherche, à la gestion des collectictions, à la communication, à l&#8217;administration ou à l&#8217;éducation.</p>
 			</article>
 		</div>
 	</section>
@@ -143,7 +143,7 @@
 	<section id="expo">
 		<div class="layout-horizontal">
 			<h2>Exposition</h2>
-			<p class="location"><strong>École Multidisciplinaire de l'Image<br /> UQO - Pavillon Lucien-Brault</strong></p>
+			<p class="location"><strong>École multidisciplinaire de l&#8217;image<br /> UQO - Pavillon Lucien-Brault</strong></p>
 			<p class="date"><time datetime="2014-04-24T17:00"><span class="day">24 avril</span> <span class="time">17&nbsp;h–21&nbsp;h</span></time></p>
 		</div>
 		<div id="map"></div>
@@ -172,19 +172,19 @@
 		<section>
 			<h2>Résaux sociaux</h2>
 			<ul>
-				<li><a href="http://www.facebook.com/pages/%C3%89cole-Multidisciplinaire-de-lImage/164233886993485" title="L'ÉMI sur Facebook">Facebook</a></li>
-				<li><a href="http://twitter.com/EMI_uqo" title="L'ÉMI sur Twitter">Twitter</a></li>
-				<li><a href="https://plus.google.com/109195428509892481796/posts" title="L'ÉMI sur Google+">Google+</a></li>
+				<li><a href="http://www.facebook.com/pages/%C3%89cole-Multidisciplinaire-de-lImage/164233886993485" title="L&#8217;ÉMI sur Facebook">Facebook</a></li>
+				<li><a href="http://twitter.com/EMI_uqo" title="L&#8217;ÉMI sur Twitter">Twitter</a></li>
+				<li><a href="https://plus.google.com/109195428509892481796/posts" title="L&#8217;ÉMI sur Google+">Google+</a></li>
 			</ul>
 		</section>
 		<section>
 			<h2>Contact</h2>
-			<a class="logo-emi" href="http://uqo.ca/emi/">École Multidisciplinaire de l'Image</a>
+			<a class="logo-emi" href="http://uqo.ca/emi/">École multidisciplinaire de l&#8217;image</a>
 			<p><strong>Université du Québec en Outaouais</strong><br />
 				Pavillon Lucien-Brault<br />
 				101, rue Saint-Jean-Bosco<br />
 				Gatineau (secteur Hull)</p>
-				<p>Créé par l'ÉMI<br /> © 2014 <a href="http://uqo.ca/emi/">École Multidisciplinaire de l'Image</a></p>
+				<p>Créé par l&#8217;ÉMI<br /> © 2014 <a href="http://uqo.ca/emi/">École multidisciplinaire de l&#8217;image</a></p>
 		</section>
 	</footer>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
